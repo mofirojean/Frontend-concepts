a827a69a379d06fdfd6056a5c967b2d877358048
Mofiro Jean <mofirojean@gmail.com>
2022-09-22 18:34:15 +0100

    implemented a new navbar

---
+++ Navbars and Sidebars/Navbar/Nav5/js/script.js
@@ -0,0 +1,8 @@
+// variable
+const icon = document.getElementById("icon");
+const ul = document.querySelector("ul")
+
+// ading event listener
+icon.addEventListener("click", () => {
+    ul.classList.toggle("show")
+})
