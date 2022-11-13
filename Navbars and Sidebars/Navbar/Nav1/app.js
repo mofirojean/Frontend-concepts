4cd23cbf8c1ee1a4502c0228ca87d49c31c2a657
Mofiro Jean <93985482+mofirojean@users.noreply.github.com>
2022-11-13 05:51:58 +0100

    feat: removed navbar/sidebar

--- Navbars and Sidebars/Navbar/Nav1/app.js
+++
@@ -1,5 +0,0 @@
- let hamburger = document.querySelector(".hamburger");
- hamburger.addEventListener('click', function () {
-     let navBar = document.querySelector('.nav-bar');
-     navBar.classList.toggle('active');
- })
--- Navbars and Sidebars/Navbar/Nav5/js/script.js
+++
@@ -1,8 +0,0 @@
-// variable
-const icon = document.getElementById("icon");
-const ul = document.querySelector("ul")
-
-// ading event listener
-icon.addEventListener("click", () => {
-    ul.classList.toggle("show")
-})
